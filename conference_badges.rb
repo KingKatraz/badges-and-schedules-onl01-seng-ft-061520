61783ec1802cee55911726b6831a2467eb6b4a7f
Hal H Nader <halnader89@gmail.com>
2020-06-26 13:52:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(badge)
+  badges = []
+  
+  badge.each {|name| badges << "Hello, my name is #{name}."}
+    
+  badges
+end
+
+def assign_rooms(names)
+  
+  room_number = []
+  
+  counter = 0
+  names.each do |name|
+  counter += 1
+  room_number << "Hello, #{name}! You'll be assigned to room #{counter}!"
+end
+room_number
+end
+
+
+
